fd99f527dd0037fa277dd552992bbb930c216817
Ivan Djurdjevac <djurdjevac@gmail.com>
2021-12-28 18:11:58 +0100

    version chabge

--- src/Boot/bootstrap.php
+++ src/Boot/bootstrap.php
@@ -5,7 +5,7 @@
 
     global $sdkName, $thisSdkVersion, $lb_plugins;
 
-    $thisSdkVersion = '1.0.0';
+    $thisSdkVersion = '1.0.1';
     $sdkName = 'LicenseBridgeSDK';
     $active_plugins = get_option('active_plugins');
     $lb_plugins = get_option('lb_registered_plugins');
